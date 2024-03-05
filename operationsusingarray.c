0732c4edd785978721ddc2bae100a8234a58c0b3
Dhruv Mundada <129663105+dd-25@users.noreply.github.com>
2024-03-05 14:06:15 +0530

    using array and using linkedlist done

---
+++ operationsusingarray.c
@@ -0,0 +1,265 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct array
+{
+    int size;
+    int *arr;
+};
+
+struct division
+{
+    struct array *quotient;
+    struct array *remainder;
+};
+
+int max(int a,int b)
+{
+    return a>b?a:b;
+}
+
+struct array* trim(struct array *num)
+{
+    int i = 0;
+    int j = 0;
+    while(i<num->size && num->arr[i]==0)
+    i++;
+    int sz = num->size-i;
+    struct array *temp = (struct array*)calloc(1,sizeof(struct array));
+    if(sz == 0)
+    {
+        temp->size = 1;
+        temp->arr = (int*)calloc(1,sizeof(int));
+        temp->arr[0] = 0;
+        return temp;
+    }
+    temp->size = sz;
+    temp->arr = (int*)calloc(sz,sizeof(int));
+    while(i<num->size)
+    {
+        temp->arr[j] = num->arr[i];
+        i++;
+        j++;
+    }
+    return temp;
+}
+
+bool greaterarr(struct array *num1,struct array *num2)
+{
+    num1 = trim(num1);
+    num2 = trim(num2);
+    if(num1->size != num2->size)
+    return num1->size > num2->size;
+    for(int i=0;i<num1->size;i++)
+    {
+        if(num1->arr[i] != num2->arr[i])
+        return num1->arr[i] > num2->arr[i];
+    }
+    return false;
+}
+
+bool equalto(struct array *num1,struct array *num2)
+{
+    int i = 0;
+    int j = 0;
+    while(i<num1->size && num1->arr[i]==0)
+    i++;
+    while(num2->size && num2->arr[j]==0)
+    j++;
+    if(num1->size-i != num2->size-j)
+    return false;
+    while(i<num1->size && j<num2->size)
+    {
+        if(num1->arr[i] != num2->arr[j])
+        return false;
+        i++;
+        j++;
+    }
+    return true;
+}
+
+struct array* takeinput()
+{
+    printf("Enter the number of digits in number\n");
+    struct array *num = (struct array*)calloc(1,sizeof(struct array));
+    scanf("%d",&(num->size));
+    num->arr = (int*)calloc(num->size,sizeof(int));
+    printf("Enter the digits with space\n");
+    for(int i=0;i<num->size;i++)
+    {
+        scanf("%d",&(num->arr[i]));
+    }
+    return trim(num);
+}
+
+void printarr(struct array* num)
+{
+    for(int i=0;i<num->size;i++)
+    {
+        printf("%d ",num->arr[i]);
+    }
+    printf("\n");
+    return;
+}
+
+struct array* addition(struct array* num1, struct array* num2)
+{
+    struct array* add = (struct array*)calloc(1,sizeof(struct array));
+    add->size = max(num1->size,num2->size) + 1;
+    add->arr = (int*)calloc(add->size,sizeof(int));
+    int i = num1->size-1;
+    int j = num2->size-1;
+    int k = add->size-1;
+    int sum = 0;
+    int carry = 0;
+    while(i>=0 && j>=0)
+    {
+        sum = num1->arr[i]+num2->arr[j];
+        add->arr[k] = sum%10 + carry;
+        carry = sum/10;
+        i--;
+        j--;
+        k--;
+    }
+    while(i>=0)
+    {
+        sum = num1->arr[i];
+        add->arr[k] = sum%10 + carry;
+        carry = sum/10;
+        i--;
+        k--;
+    }
+    while(j>=0)
+    {
+        sum = num2->arr[j];
+        add->arr[k] = sum%10 + carry;
+        carry = sum/10;
+        j--;
+        k--;
+    }
+    add->arr[k] = carry;
+    return trim(add);
+}
+
+struct array* subtraction(struct array *num1,struct array *num2)
+{
+    struct array* sub = (struct array*)calloc(1,sizeof(struct array));
+    sub->size = max(num1->size,num2->size);
+    sub->arr = (int*)calloc(sub->size,sizeof(int));
+    if(greaterarr(num2,num1))
+    {
+        sub->size = 0;
+        free(sub->arr);
+        printf("Cannot Subract\n");
+        return sub;
+    }
+    int i = num1->size-1;
+    int j = num2->size-1;
+    int k = sub->size-1;
+    int borrow = 0;
+    while(i>=0 && j>=0)
+    {
+        int diff = (num1->arr[i]-borrow)-num2->arr[j];
+        if(diff<0)
+        {
+            diff += 10;
+            borrow = 1;
+        }
+        else
+        {
+            borrow = 0;
+        }
+        sub->arr[k] = diff;
+        i--;
+        j--;
+        k--;
+    }
+    while(i>=0)
+    {
+        sub->arr[k] = num1->arr[i]-borrow;
+        borrow = 0;
+        i--;
+        k--;
+    }
+    return trim(sub);
+}
+
+struct array* multiplication(struct array *num1,struct array *num2)
+{
+    struct array *mul = (struct array*)calloc(1,sizeof(struct array));
+    mul->size = num1->size+num2->size;
+    mul->arr = (int*)calloc(mul->size,sizeof(int));
+    int i = num2->size-1;
+    while(i>=0)
+    {
+        int j = num1->size-1;
+        int carry1 = 0;
+        int carry2 = 0;
+        int k = num1->size+i;
+        while(j>=0)
+        {
+            int mult = num1->arr[j]*num2->arr[i]+carry1;
+            carry1 = mult/10;
+            mul->arr[k] += mult%10 + carry2;
+            carry2 = mul->arr[k]/10;
+            mul->arr[k] %= 10;
+            j--;
+            k--;
+        }
+        while(carry1)
+        {
+            int mult = carry1;
+            carry1 = mult/10;
+            mul->arr[k] += mult%10 + carry2;
+            carry2 = mul->arr[k]/10;
+            mul->arr[k] %= 10;
+        }
+        if(carry2)
+        mul->arr[k] = carry2;
+        i--;
+    }
+    return trim(mul);
+}
+
+struct division* division(struct array *num1,struct array* num2)
+{
+    struct division *div = (struct division*)calloc(1,sizeof(struct division));
+    div->quotient = (struct array*)calloc(1,sizeof(struct array));
+    div->remainder = (struct array*)calloc(1,sizeof(struct array));
+    div->quotient->size = 1;
+    div->quotient->arr = (int*)calloc(1,sizeof(int));
+    div->remainder = num1;
+    struct array *qadd = (struct array*)calloc(1,sizeof(struct array));
+    qadd->size = 1;
+    qadd->arr = (int*)calloc(1,sizeof(int));
+    qadd->arr[0] = 1;
+    while(greaterarr(div->remainder,num2) || equalto(div->remainder,num2))
+    {
+        div->quotient = addition(div->quotient,qadd);
+        div->remainder = subtraction(div->remainder,num2);
+    }
+    div->quotient = trim(div->quotient);
+    div->remainder = trim(div->remainder);
+    return div;
+}
+
+int main()
+{
+    printf("Enter the details of first number\n");
+    struct array *num1 = takeinput();
+    printf("Enter the details of second number\n");
+    struct array *num2 = takeinput();
+    struct array *add = addition(num1,num2);
+    struct array *sub = subtraction(num1,num2);
+    struct array *mul = multiplication(num1,num2);
+    struct division *div = division(num1,num2);
+    printf("First Number:   ");printarr(num1);
+    printf("Second Number:  ");printarr(num2);
+    printf("Addition:       ");printarr(add);
+    printf("Subtraction:    ");printarr(sub);
+    printf("Multiplication: ");printarr(mul);
+    printf("Quotient:       ");printarr(div->quotient);
+    printf("Remainder:      ");printarr(div->remainder);
+    return 0;
+}
---
+++ operationsusingll.c
@@ -0,0 +1,355 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct number
+{
+    int size;
+    struct node *head;
+};
+
+struct division
+{
+    struct number *quotient;
+    struct number *remainder;
+};
+
+int max(int a,int b)
+{
+    return a>b?a:b;
+}
+
+void trim(struct number *num)
+{
+    int i = 0;
+    struct node *curr = num->head;
+    while(curr!=NULL && curr->data==0)
+    {
+        curr = curr->next;
+        i++;
+    }
+    num->size = num->size-i;
+    if(curr==NULL)
+    {
+        struct node *temp = (struct node*)malloc(sizeof(struct node));
+        temp->data = 0;
+        temp->next = NULL;
+        num->head = temp;
+        num->size = 1;
+        return;
+    }
+    num->head = curr;
+    return;
+}
+
+bool greaterll(struct number *num1,struct number *num2)
+{
+    trim(num1);
+    trim(num2);
+    if(num1->size!=num2->size)
+    return num1->size > num2->size;
+    struct node *temp1 = num1->head;
+    struct node *temp2 = num2->head;
+    while(temp1!=NULL)
+    {
+        if(temp1->data!=temp2->data)
+        return temp1->data > temp2->data;
+        temp1 = temp1->next;
+        temp2 = temp2->next;
+    }
+    return false;
+}
+
+bool equalto(struct number *num1,struct number *num2)
+{
+    trim(num1);
+    trim(num2);
+    if(num1->size != num2->size)
+    return false;
+    struct node *temp1 = num1->head;
+    struct node *temp2 = num2->head;
+    while(temp1!=NULL)
+    {
+        if(temp1->data!=temp2->data)
+        return false;
+        temp1 = temp1->next;
+        temp2 = temp2->next;
+    }
+    return true;
+}
+
+void printll(struct number *num)
+{
+    struct node *temp = num->head;
+    for(int i=0;i<num->size;i++)
+    {
+        printf("%d ",temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+    return;
+}
+
+void insert_begin(struct node **num,int x)
+{
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = x;
+    temp->next = *num;
+    *num = temp;
+    return;
+}
+
+void insert_end(struct node** num,int x)
+{
+    struct node *temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = x;
+    temp->next = NULL;
+    if(*num == NULL)
+    {
+        *num = temp;
+        return;
+    }
+    struct node *curr = *num;
+    while(curr->next!=NULL)
+    {
+        curr = curr->next;
+    }
+    curr->next = temp;
+    return;
+}
+
+void reverse(struct node **num)
+{
+    struct node *prev = NULL;
+    struct node *curr = *num;
+    struct node *next = NULL;
+    while(curr!=NULL)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    *num = prev;
+    return;
+}
+
+struct number* takeinput()
+{
+    printf("Enter the number of digits\n");
+    int n;
+    scanf("%d",&n);
+    struct node *num = NULL;
+    struct number *nm = (struct number*)malloc(sizeof(struct number));
+    printf("Enter the number with space\n");
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        scanf("%d",&x);
+        insert_end(&num,x);
+    }
+    nm->size = n;
+    nm->head = num;
+    trim(nm);
+    return nm;
+}
+
+struct number *addition(struct number *num1,struct number *num2)
+{
+    struct number *add = (struct number*)malloc(sizeof(struct number));
+    add->size = 0;
+    add->head = NULL;
+    reverse(&num1->head);
+    reverse(&num2->head);
+    struct node *curr1 = num1->head;
+    struct node *curr2 = num2->head;
+    int sum = 0;
+    int carry = 0;
+    while(curr1!=NULL && curr2!=NULL)
+    {
+        sum = curr1->data+curr2->data+carry;
+        carry = sum/10;
+        insert_end(&add->head,sum%10);
+        add->size++;
+        curr1 = curr1->next;
+        curr2 = curr2->next;
+    }
+    while(curr1!=NULL)
+    {
+        sum = curr1->data+carry;
+        carry = sum/10;
+        insert_end(&add->head,sum%10);
+        add->size++;
+        curr1 = curr1->next;
+    }
+    while(curr2!=NULL)
+    {
+        sum = curr2->data+carry;
+        carry = sum/10;
+        insert_end(&add->head,sum%10);
+        add->size++;
+        curr2 = curr2->next;
+    }
+    if(carry)
+    {
+        insert_end(&add->head,carry);
+        add->size++;
+    }
+    reverse(&num1->head);
+    reverse(&num2->head);
+    reverse(&add->head);
+    trim(add);
+    return add;
+}
+
+struct number* subtraction(struct number *num1,struct number *num2)
+{
+    struct number *sub = (struct number*)malloc(sizeof(struct number));
+    sub->size = 0;
+    sub->head = NULL;
+    if(greaterll(num2,num1))
+    {
+        printf("Cannot Subtract\n");
+        return sub;
+    }
+    reverse(&num1->head);
+    reverse(&num2->head);
+    struct node *curr1 = num1->head;
+    struct node *curr2 = num2->head;
+    int borrow = 0;
+    while(curr2!=NULL)
+    {
+        int diff = (curr1->data-borrow)-curr2->data;
+        if(diff<0)
+        {
+            diff += 10;
+            borrow = 1;
+        }
+        else
+        borrow = 0;
+        insert_end(&sub->head,diff);
+        sub->size++;
+        curr1 = curr1->next;
+        curr2 = curr2->next;
+    }
+    while(curr1!=NULL)
+    {
+        insert_end(&sub->head,curr1->data-borrow);
+        borrow = 0;
+        sub->size++;
+        curr1 = curr1->next;
+    }
+    reverse(&num1->head);
+    reverse(&num2->head);
+    reverse(&sub->head);
+    trim(sub);
+    return sub;
+}
+
+struct number* multiplication(struct number* num1,struct number* num2)
+{
+    struct number *mul = (struct number*)malloc(sizeof(struct number));
+    mul->size = 1;
+    mul->head = (struct node*)malloc(sizeof(struct node));
+    mul->head->data = 0;
+    mul->head->next = NULL;
+    reverse(&num1->head);
+    reverse(&num2->head);
+    struct node *curr1 = num1->head;
+    struct node *curr2 = num2->head;
+    struct number *temp[num2->size];
+    for(int i=0;i<num1->size+num2->size;i++)
+    {
+        temp[i] = (struct number*)malloc(sizeof(struct number));
+        temp[i]->size = 0;
+        temp[i]->head = NULL;
+    }
+    int j = 0;
+    while(curr2!=NULL)
+    {
+        curr1 = num1->head;
+        int carry = 0;
+        for(int k=0;k<j;k++)
+        {
+            insert_end(&temp[j]->head,0);
+            temp[j]->size++;
+        }
+        while(curr1!=NULL)
+        {
+            int mult = curr2->data*curr1->data+carry;
+            carry = mult/10;
+            insert_end(&temp[j]->head,mult%10);
+            temp[j]->size++;
+            curr1 = curr1->next;
+        }
+        while(carry)
+        {
+            insert_end(&temp[j]->head,carry%10);
+            temp[j]->size++;
+            carry /= 10;
+        }
+        j++;
+        curr2 = curr2->next;
+    }
+    for(int i=0;i<num2->size;i++)
+    {
+        reverse(&temp[i]->head);
+        mul = addition(mul,temp[i]);
+    }
+    reverse(&num1->head);
+    reverse(&num2->head);
+    trim(mul);
+    return mul;
+}
+
+struct division* division(struct number *num1,struct number *num2)
+{
+    struct division *div = (struct division*)malloc(sizeof(struct division));
+    div->quotient = (struct number*)malloc(sizeof(struct number));
+    div->remainder = (struct number*)malloc(sizeof(struct number));
+    div->quotient->size = 1;
+    div->quotient->head = (struct node*)malloc(sizeof(struct node));
+    div->quotient->head->data = 0;
+    div->quotient->head->next = NULL;
+    struct number *qadd = (struct number*)malloc(sizeof(struct number));
+    qadd->size = 1;
+    qadd->head = (struct node*)malloc(sizeof(struct node));
+    qadd->head->data = 1;
+    qadd->head->next = NULL;
+    struct number *temp = num1;
+    while(greaterll(temp,num2) || equalto(temp,num2))
+    {
+        temp = subtraction(temp,num2);
+        div->quotient = addition(div->quotient,qadd);
+    }
+    div->remainder = temp;
+    trim(div->quotient);
+    trim(div->remainder);
+    return div;
+}
+
+int main()
+{
+    printf("Enter the first number\n");
+    struct number *num1 = takeinput();
+    printf("Enter the second number\n");
+    struct number *num2 = takeinput();
+    struct number *add = addition(num1,num2);
+    struct number *sub = subtraction(num1,num2);
+    struct number *mul = multiplication(num1,num2);
+    struct division *div = division(num1,num2);
+    printf("First Number:   ");printll(num1);
+    printf("Second Number:  ");printll(num2);
+    printf("Addition:       ");printll(add);
+    printf("Subtraction:    ");printll(sub);
+    printf("Multiplication: ");printll(mul);
+    printf("Quotient:       ");printll(div->quotient);
+    printf("Remainder:      ");printll(div->remainder);
+    return 0;
+}
